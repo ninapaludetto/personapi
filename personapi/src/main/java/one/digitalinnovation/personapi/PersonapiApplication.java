27e42deb41f31ad3b946dfef79c57b91c660ed5e
ninapaludetto <ninapaludetto@hotmail.com>
2021-07-23 11:40:23 -0300

    Initial commit

---
+++ personapi/src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personapi;
+
+import
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;...
+
+
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
---
+++ personapi/src/main/java/one/digitalinnovation/personapi/dto/package-info.java
@@ -0,0 +1,1 @@
+package one.digitalinnovation.personapi.dto;
